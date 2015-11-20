8132dff4526a5ecd84d5cc739854a09eeb0ffa00
Emilio Junior <ejuniorasas@gmail.com>
2015-11-20 18:17:15 -0200

    inventory

---
+++ fair/app/models/application_dao.rb
@@ -0,0 +1,17 @@
+module ApplicationDao
+
+	def list_products
+      @produtos = Complement.where(tipo: 'Produto').order(:nome)
+    end
+
+    def list_clients
+      @clientes = Complement.where(tipo: 'Cliente')
+      @clientes.push(Complement.where(tipo: 'Cliente/Fornec.'))
+    end
+
+    def list_fornecedor
+      @fornecedores = Complement.where(tipo: 'Fornecedor')
+      @fornecedores.push(Complement.where(tipo: 'Cliente/Fornec.'))
+    end
+
+end
